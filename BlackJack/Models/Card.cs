761594e97939ccdd461612a1df4c0216692b5c69
Matt <msb13200@outlook.com>
2014-08-15 18:15:29 -0400

    DeckCard

---
+++ BlackJack/Models/Card.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlackJack.Models
+{
+    public enum Suit
+    {
+        Hearts,diamonds,clubs,spades
+    };
+    public enum FaceValue
+    {
+        ace,two,three,four,five,six,seven,eight,nine,ten,jack,queen,king
+    };
+    public class Card
+    {
+        public Card(Suit suit, FaceValue faceValue)
+        {
+            this.suit = suit;
+            this.faceValue = faceValue;
+        }
+        private Suit suit {get;set;}
+        private FaceValue faceValue { get; set; }
+    }
+}
---
+++ BlackJack/Models/Deck.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BlackJack.Models
+{
+    public static class Deck
+    {
+        private static List<Card> deck { get; set; }
+        public static List<Card> BuildDeck()
+        {
+            foreach (Suit suit in Enum.GetValues(typeof(Suit)))
+            {
+                foreach (FaceValue faceValue in Enum.GetValues(typeof(FaceValue)))
+                {
+                    deck.Add(new Card(suit, faceValue));
+                }
+            }
+            return deck;
+        }
+    }
+}
